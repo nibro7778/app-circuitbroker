67e17c0d8f0567a2d5b2aeb37c14ced3d1789fef
Niraj Trivedi <mailme7778@gmail.com>
2018-03-09 21:07:07 +1100

    Added swagger nuget package and updated routeprefix and route for Hello world API

---
+++ ExternalService.WebAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Web.Http;
+
+namespace ExternalService.WebAPI.Controllers
+{
+    /// <summary>
+    /// List of API with differnt "Hello World" functionality
+    /// </summary>
+    [RoutePrefix("HelloWorld/api/v1")]
+    public class HelloWorldController : ApiController
+    {
+        /// <summary>
+        /// This API simply retrun "Hello World" 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Get")]        
+        public IHttpActionResult GetHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
